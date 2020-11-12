5fcd716387c9f8cc3a5428c228f93c9de47897f2
jasoben <jasoben@gmail.com>
2020-11-12 14:01:41 -0500

    first commit

---
+++ UVAUnity3D Three Doors Project/Assets/Scripts/TurnCubeOnOrOff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCubeOnOrOff : MonoBehaviour
+{
+    public Material onMaterial, offMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().material = onMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<MeshRenderer>().material = offMaterial;
+    }
+}
