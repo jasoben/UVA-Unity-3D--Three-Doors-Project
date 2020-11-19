5ee9635d7d7f0a221a69838941025850b221bcc2
jasoben <jasoben@gmail.com>
2020-11-19 12:58:46 -0500

    rigidbodies and player controllers

---
+++ UVAUnity3D Three Doors Project/Assets/Scripts/RigidBody Experiments/ForceMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMove : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    public Vector3 forceOnBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rigidbody.AddForce(forceOnBall);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddRelativeForce(forceOnBall);
+        }
+    }
+}
