16b51c25e6fd3298b6f6815b92c828d018ec1633
jasoben <jasoben@gmail.com>
2021-02-13 10:22:30 -0500

    getcomponents and prefabs

---
+++ UVAUnity3D Three Doors Project/Assets/Scripts/GetComponent+Prefabs/Rotator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private float storedRotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        storedRotationSpeed = rotationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+
+    public void StartStopRotation()
+    {
+        if (rotationSpeed == 0)
+        {
+            rotationSpeed = storedRotationSpeed;
+        }
+        else
+        {
+            storedRotationSpeed = rotationSpeed;
+            rotationSpeed = 0;
+        }
+    }
+}
